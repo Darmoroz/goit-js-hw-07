5bd275e439eae8666229eb352fdf0ecba7104833
Darmoroz <darmoroz99@gmail.com>
2022-10-12 21:59:02 +0300

    refactoring 01-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,37 +4,56 @@ const refs = {
 	galerry: document.querySelector(".gallery"),
 };
 
-const markupGalerryItems = galleryItems.map(createGalleryItem).join("");
+refs.galerry.insertAdjacentHTML(
+	"afterbegin",
+	createGalleryItemsMarkup(galleryItems),
+);
+refs.galerry.addEventListener("click", onShowModal);
 
-function createGalleryItem(obj) {
-	return `<div class="gallery__item">
-	<a class="gallery__link" href="${obj.original}">
+function createGalleryItemsMarkup(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+	<a class="gallery__link" href="${original}">
 		<img
 			class="gallery__image"
-			src="${obj.preview}"
-			data-source="${obj.original}"
-			alt="${obj.description}"
+			src="${preview}"
+			data-source="${original}"
+			alt="${description}"
 		/>
 	</a>
 </div>`;
+		})
+		.join("");
 }
 
 function onShowModal(e) {
 	e.preventDefault();
-
 	const condition = e.target.closest(".gallery__item");
-
 	if (!condition) return;
-
 	const instance = basicLightbox.create(
 		`<img src="${e.target.dataset.source}">`,
 	);
 	instance.show();
 	window.addEventListener("keydown", (e) => {
-		if (e.code !== "Escape") return;
-		instance.close();
+		if (e.code === "Escape") {
+			instance.close();
+		}
 	});
 }
 
-refs.galerry.insertAdjacentHTML("afterbegin", markupGalerryItems);
-refs.galerry.addEventListener("click", onShowModal);
+//* Another case
+// const markupGalerryItems = galleryItems.map(createGalleryItem).join("");
+// function createGalleryItem(obj) {
+// 	return `<div class="gallery__item">
+// 	<a class="gallery__link" href="${obj.original}">
+// 		<img
+// 		class="gallery__image"
+// 		src="${obj.preview}"
+// 		data-source="${obj.original}"
+// 		alt="${obj.description}"
+// 		/>
+// 		</a>
+// 		</div>`;
+// }
+// refs.galerry.insertAdjacentHTML("afterbegin", markupGalerryItems);
